46da0e2f0edf340f03c74590a715313085cdac9e
Kyan L.C. <157541674+kyabetes@users.noreply.github.com>
2024-09-13 14:16:20 -0400

    Create PercolationStats.java

---
+++ Percolation/PercolationStats.java
@@ -0,0 +1,67 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class PercolationStats {
+    private static final double confidence_95 = 1.96;
+    // I am now grateful I took AP stats...
+    private final double[] thresholds;
+    private final int trials;
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("n and trials must be greater than 0");
+        }
+        this.trials = trials;
+        this.thresholds = new double[trials];
+
+        for (int t = 0; t < trials; t++) {
+            Percolation percolation = new Percolation(n);
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniformInt(1, n + 1);
+                int col = StdRandom.uniformInt(1, n + 1);
+                if (!percolation.isOpen(row, col)) {
+                    percolation.open(row, col);
+                }
+            }
+            thresholds[t] = (double) percolation.numberOfOpenSites() / (n * n);
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        return mean() - ((confidence_95 * stddev()) / Math.sqrt(trials));
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return mean() + ((confidence_95 * stddev()) / Math.sqrt(trials));
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+
+        Stopwatch stopwatch = new Stopwatch();
+        PercolationStats stats = new PercolationStats(n, trials);
+        double elapsedTime = stopwatch.elapsedTime();
+
+        System.out.printf("mean()           = %f\n", stats.mean());
+        System.out.printf("stddev()         = %f\n", stats.stddev());
+        System.out.printf("confidenceLow()  = %f\n", stats.confidenceLow());
+        System.out.printf("confidenceHigh() = %f\n", stats.confidenceHigh());
+        System.out.printf("elapsed time     = %.3f\n", elapsedTime);
+    }
+}
